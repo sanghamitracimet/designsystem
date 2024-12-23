1c18d7fe5d3a84b7fa7b77153f8277b4ed9a145a
Divya Punjabi <divya.punjabi@cimet.com.au>
2024-12-23 09:47:50 +0530

    initial commit

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,7 @@
+const index = () => {
+  return (
+    <div>dashboard page</div>
+  )
+}
+
+export default index
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+const index = () => {
+  return (
+    <div>root page</div>
+  )
+}
+
+export default index
