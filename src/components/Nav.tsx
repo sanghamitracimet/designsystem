894788793f227052aacb9da71e40c72ae626b831
sanghamitracimet <sanghamitra.satpathy@cimet.com.au>
2024-12-27 14:29:58 +0530

    feat: updated chartTab css and sideNav css

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Link from "next/link";
+import { GiHamburgerMenu } from "react-icons/gi";
+
 
 const Nav = () => {
   return (
-    <div>
-      
+    <div className='w-full p-4'>
+      <div className="flex gap-4">
+        <div>
+          <GiHamburgerMenu  className="w-6 h-6" />
+        </div>
+        <Link href="/">Home</Link>
+        <Link href="/">Contact</Link>
+      </div>
     </div>
   )
 }
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,23 +1,31 @@
 // src/components/SideNav.tsx
 import Link from 'next/link';
+import { AiFillDashboard } from "react-icons/ai";
+import { BiSolidPieChartAlt2 } from "react-icons/bi";
+import { FaTable } from "react-icons/fa";
+import { LuListTodo } from "react-icons/lu";
 
 const SideNav = () => {
   return (
-    <div className="fixed flex flex-col w-56 h-screen bg-primary-sidebar text-white p-4 z-10">
+    <div className="fixed flex flex-col w-56 h-screen bg-primary-sidebar text-lightGray p-4 z-10">
       <div>
         <h2 className="text-2xl mb-8">My App</h2>
-        <ul className="space-y-4">
-          <li>
-            <Link href="/">Dashboard</Link>
+        <ul className="space-y-8">
+          <li className='flex gap-2 items-center'>
+            <AiFillDashboard className='w-6 h-6' />
+            <Link className='text-md' href="/">Dashboard</Link>
           </li>
-          <li>
-            <Link href="/charts">Chart</Link>
+          <li className='flex gap-2 items-center'>
+            <BiSolidPieChartAlt2 className='w-6 h-6' />
+            <Link className='text-md' href="/charts">Chart</Link>
           </li>
-          <li>
-            <Link href="/datatable">Data Table</Link>
+          <li className='flex gap-2 items-center'>
+            <FaTable className='w-6 h-6' />
+            <Link className='text-md' href="/datatable">Data Table</Link>
           </li>
-          <li>
-            <Link href="/todo">Todo</Link>
+          <li className='flex gap-2 items-center'>
+            <LuListTodo className='w-6 h-6' />
+            <Link className='text-md' href="/todo">Todo</Link>
           </li>
         </ul>
       </div>
